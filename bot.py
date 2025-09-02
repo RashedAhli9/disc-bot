a9266942a436e73ec328834358e0b2b1ef6fa94e
RashedAhli9 <Rashedahli988@gmail.com>
2025-09-02 21:34:20 +0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands, tasks
+import datetime
+import pytz
+
+# Use UTC timezone
+MY_TIMEZONE = "UTC"
+channel_id = 1328658110897983549  # <-- Replace with your channel ID
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    check_time.start()
+
+@tasks.loop(minutes=1)
+async def check_time():
+    tz = pytz.timezone(MY_TIMEZONE)
+    now = datetime.datetime.now(tz)
+
+    # Tuesday = 1, Friday = 4  (Python weekday: Monday = 0)
+    if now.weekday() in [1, 4]:
+        if now.hour in [0, 8, 16] and now.minute == 0:
+            channel = bot.get_channel(channel_id)
+            await channel.send("abyss is starting in 15 minutes!")
+
+bot.run("MTQxMjQ2OTYzNzk5NTY5NjE5OA.GktOLr.VWqljMNm9XqgQuqHHipWk85qvb96SyK981_2_k")  # <-- Replace with your bot token
